7805bfe7807c093402f08c3d8b396390efd181e2
Vaseem Mohammed <vaseem.mohammed@ugamsolutions.com>
2014-12-09 23:36:48 +0530

    angular project created

---
+++ client/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scenetagApp
+ * @description
+ * # scenetagApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scenetagApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scenetagApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scenetagApp
+ */
+angular.module('scenetagApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
